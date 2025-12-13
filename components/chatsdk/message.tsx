397c22cad99619c2f1db5dc2e4d9aedade29871b
bubune99 <bubune99@gmail.com>
2025-12-13 17:10:45 -0500

    server side error fix

--- components/chatsdk/message.tsx
+++ components/chatsdk/message.tsx
@@ -46,9 +46,9 @@ const PurePreviewMessage = ({
 }) => {
   const [mode, setMode] = useState<"view" | "edit">("view");
 
-  const attachmentsFromMessage = message.parts.filter(
+  const attachmentsFromMessage = message.parts?.filter(
     (part) => part.type === "file"
-  );
+  ) || [];
 
   useDataStream();
 
